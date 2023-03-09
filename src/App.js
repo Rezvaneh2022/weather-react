6a5d9567bc4871d86cf7560cea798457ededa6c8
Rezvaneh2022 <116438852+Rezvaneh2022@users.noreply.github.com>
2023-03-09 13:20:45 +0330

    added api call

--- src/App.js
+++ src/App.js
@@ -1,55 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./App.css";
 import Search from "./Search";
 
 export default function App() {
-  return (
-    <div className="container">
-      <div className="wholePage">
-        <span>
-          <Search />
-        </span>
-        <div>
-          <div className="row firstSection">
-            <div className="col-4">
-              <img src="#" id="icon" alt=" icon" />
-            </div>
-            <div class="col-4 city-name">
-              <h1 id="city">Berlin</h1>
-            </div>
-            <div className="col-4">
-              <div id="date" class="date"></div>
-            </div>
-          </div>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
-          <span className="temperatureSection">
-            <strong id="temperature" class="weather-temperature"></strong>
-            <span className="units">°C</span>
+  function handleResponse(response) {
+    console.log(response);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      city: response.data.city,
+      icon: `${response.data.condition.icon_url}`,
+      description: response.data.condition.description,
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="container">
+        <div className="wholePage">
+          <span>
+            <Search />
           </span>
+          <div>
+            <div className="row firstSection">
+              <div className="col-4">
+                <img src={weatherData.icon} id="icon" alt=" icon" />
+              </div>
+              <div class="col-4 city-name">
+                <h1 id="city">{weatherData.city}</h1>
+              </div>
+              <div className="col-4">
+                <div id="date" class="date"></div>
+              </div>
+            </div>
 
-          <ul>
-            <li>
-              Humidity:
-              <span id="humidity"></span>
-            </li>
+            <span className="temperatureSection">
+              <strong id="temperature" class="weather-temperature">
+                {Math.round(weatherData.temperature)}
+              </strong>
+              <span className="units">°C</span>
+            </span>
 
-            <li>
-              Wind: <span id="wind"></span> km/h
-            </li>
-          </ul>
+            <ul>
+              <li>
+                Description:{" "}
+                <span id="description" class="text-muted">
+                  {weatherData.description}
+                </span>
+              </li>
+              <li>
+                Humidity:{" "}
+                <span id="humidity" class="text-muted">
+                  {Math.round(weatherData.humidity)}
+                </span>
+              </li>
+
+              <li>
+                Wind:{" "}
+                <span id="wind" className="text-muted !imortant">
+                  {Math.round(weatherData.wind)}
+                </span>{" "}
+                km/h
+              </li>
+            </ul>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+          <a
+            href="hhttp://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+            target="_blank"
+            className="gitLink"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by Rezvaneh Hosseini
         </div>
-        <div className="weather-forecast" id="forecast"></div>
-        <a
-          href="hhttp://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-          target="_blank"
-          className="gitLink"
-          rel="noreferrer"
-        >
-          Open-source code{" "}
-        </a>
-        by Rezvaneh Hosseini
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "588oc041a6e953f45658b7911bf5tb98";
+    let city = "Tehran";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return <h2>Loading the app ...</h2>;
+  }
 }
